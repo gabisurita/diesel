b922760768f0fca51486773d28248bc162fad012
Gabriela Surita <gabsurita@gmail.com>
2018-04-03 00:02:04 -0300

    feat(pg): backport PgPoint to 1.1.0

--- diesel/src/pg/mod.rs
+++ diesel/src/pg/mod.rs
@@ -31,4 +31,5 @@ pub mod data_types {
     #[doc(inline)]
     pub use super::types::money::PgMoney;
     pub use super::types::money::PgMoney as Cents;
+    pub use super::types::geometric::PgPoint;
 }
